0ca5c063f9b559e4070eb622f959e9a0b1ccd390
Birkan Özer <birkanozer@hotmail.com>
2022-10-02 01:09:39 +0300

    login page created

---
+++ kohort-gui/src/pages/DashboardPage/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import BasePage from "../BasePage";
+
+const DashboardPage = () => {
+  return (
+    <BasePage>
+      <h1>Content here</h1>
+    </BasePage>
+  );
+};
+
+export default DashboardPage;
